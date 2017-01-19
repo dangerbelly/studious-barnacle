b3b0a4124aafdf6cd222b432b035efa51eac45f0
Ryan Aguilar <raguilar@wrightesd.org>
2017-01-19 09:53:03 -0800

    Modified

--- app/views.py
+++ app/views.py
@@ -9,7 +9,9 @@ from .models import gradelevel_dataset
 from werkzeug import secure_filename
 import os
 
-app.config['UPLOAD_FOLDER'] = 'static/'
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+UPLOAD_FOLDER = os.path.join(APP_ROOT, 'static')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 app.config['ALLOWED_EXTENSIONS'] = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'csv'])
 
@@ -103,7 +105,7 @@ def upload():
         # the upload folder we setup
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
-        load_table(filename, "master1")
+        load_table('static/%s' % filename, "master1")
 
         gld = gradelevel_dataset('master1')
 
