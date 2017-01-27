bd26299271230bad467ccb18975479a2f0b2a8f0
Ryan Aguilar <itcoordinator@Ryans-MacBook-Air.local>
2017-01-27 15:15:13 -0800

    Hide and tabs

--- app/static/main.js
+++ app/static/main.js
@@ -1,13 +1,26 @@
+
+function showDiv(){
+
+			$("#myId").removeClass('hidden')
+			localStorage.setItem('show','true');
+		}
+
 $(document).ready(function() {
 
 	console.log("ready!");
+
+	//localStorage.removeItem('show','true');
+
 
 	$("form").on("submit", function() {
 		console.log("the form has been submitted")
 
 		var valueOne = $('input[name="file"]').val()
 
-		console.log(valueOne)
+		localStorage.setItem('show','true');
+
+
+		console.log(valueOne);
 
 		$.ajax({
 			type: "POST",
@@ -20,12 +33,24 @@ $(document).ready(function() {
 				$("#results").html(results.data1)
 				$("#school_number").html(results.school_number)
 				$("#schools").html(results.schools)
+    				      		    							
 			},
 			error: function(error) {
 				console.log(error);
 			}
 		});
 
+
 	});
-	
+
+		var show = localStorage.getItem('show');
+		
+		if (show ==='true'){
+			$("#myId").show()
+		}
+		console.log(show);
+
+
+
 });
+
